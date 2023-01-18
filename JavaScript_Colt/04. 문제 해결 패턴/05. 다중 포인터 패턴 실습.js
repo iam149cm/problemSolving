9f5cf26b9cb20d0a55e9800208fef8dfce041b26
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-01-18 16:16:57 +0900

    update multiple pointer pattern

--- JavaScript_Colt/04. 문제 해결 패턴/05. 다중 포인터 패턴 실습.js
+++ JavaScript_Colt/04. 문제 해결 패턴/05. 다중 포인터 패턴 실습.js
@@ -6,15 +6,21 @@
  * countUniqueValues([]) // 0
  * countUniqueValues([-2, -1, -1, 0, 1]) // 4
  */
+
+// 🌟 고유값 세기 
 function countUniqueValues1(arr) {
+    if(arr.length = 0) { return 0;}
     var i = 0;
     for(var j=1; j < arr.length; j++) {
-        // arr[i] =
-
-
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+        console.log("i-",i,', j-', j);
     }
-
+    return i + 1; // i는 인덱스 0부터 시작하므로
 }
+countUniqueValues1([1,1,1,2,2,3,4,5,5,5,6,7]); // 7
 
 
 // 내가 작성한 답변 😂
