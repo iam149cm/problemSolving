c281a274742077e6bea79bd28fb7fa7b2e0f5fc0
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-01-20 17:38:11 +0900

    add sorting algorithm

---
+++ JavaScript_Colt/07. 버블정렬/01. 정렬알고리즘 소개.js
@@ -0,0 +1,22 @@
+/** 🌟 Elementary Sorting Algorithms
+ * ✨ https://cs.slides.com/colt_steele/elementary-sorting-algorithms
+ * ✨ https://www.toptal.com/developers/sorting-algorithms
+ * 
+ * 🌟 What is sorting ?
+ * - Sorting is the process of rearranging the items in a collection. (e.g. an array) 
+ * so that the items are in some kind of order.
+ * 컬렉션의 항목을 재배열하는 과정을 의미한다. 문자열을 하나 고른 후 문자열 내의 각 문자를 정렬하는 것
+ * 
+ * ex) 
+ * - Sorting numbers from smallest to large
+ * - Sorting names alphabetically
+ * - Sorting movies based on release year
+ * 
+ * 🌟 Why do we need to learn this ?
+ * 1. Sorting is an incredibly common task, so it's good to know how it works
+ * 2. There are many different ways to sort things, and different techniques have their
+ * own advantages and disadvantages.
+ * 
+ * 👉 데이터의 상태에 따라 (Random, Nearly Sorted, Reversed, Few Unique) 정렬되는 속도가 알고리즘별로 다르다.
+ */
+ 
---
+++ JavaScript_Colt/07. 버블정렬/02. 자바스크립트 내장 정렬.js
@@ -0,0 +1,25 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+/** 🌟 Java Script has the sort() funtion
+ * 
+ * - The built-in sort method accepts an optional compartor funtion 👉 선택적 비교함수를 인자로 전달받는다.
+ * - You can use this comparator function to tell JavaScript how you want it to sort 👉 원하는 정렬 방식을 알린다.
+ * - The comparator looks at paris of elements (a and b), determines there sort order based on the return value.
+ *  -- If it returns a negative number, a should come before b. (vice versa)
+ *  -- If it returns 0, a and b are the same as far as the sort is concerned
+ */
+
+// 👉 숫자를 오름차순으로 정렬하고 싶다면
+function numberCompare (num1, num2) {
+    return num1 - num2; // 음수를 반환하면 num1이 num2앞에 온다.
+}
+
+// console.log([6, 4, 15, 10].sort(numberCompare));
+
+// 👉 string 길이의 오름차순으로 정렬하고 싶다면
+function compareByLen(str1, str2) {
+    return str1.length - str2.length;
+}
+
+const result = ["Steele", "Colt" , "Data Structures" , "Algorithms" ].sort(compareByLen);
+console.log(result);
