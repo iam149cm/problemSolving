74a5e81f8184a7b0475dcc2c2fd98a9504349086
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-01-22 23:31:51 +0900

    update bubble sort

--- JavaScript_Colt/07. 버블정렬/03. 버블 정렬.js
+++ JavaScript_Colt/07. 버블정렬/03. 버블 정렬.js
@@ -8,13 +8,35 @@
 
 // 🌟 Before we sort, we must swap.
 // ES 5
-function swap(arr, idx1, idx2) {
+function swap1(arr, idx1, idx2) {
     var temp = arr[idx1];
     arr[idx1] = arr[idx2];
     arr[idx2] = temp;
 }
 
 // ES2015
-const swap = (arr, idx1, idx2) => {
+const swap2 = (arr, idx1, idx2) => {
     [arr[idx1], [arr[idx2]] = [arr[idx2], arr[idx1]]];
 }
+
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            console.log(arr)
+            if(arr[j] > arr[j+1]) {
+                // SWAP
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+
+console.log(bubbleSort([23, 56, 12, 8]));
+// [23, 12, 8, 56]
+// [12, 8, 23, 56]
+// [8, 12, 23, 56]
